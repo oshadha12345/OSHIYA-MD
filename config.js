5eac0c169437832d59ffe0b5ed08571f56130afa
oshadha12345 <manuppriyaoshadha@gmail.com>
2025-12-01 07:56:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/oshadha12345/OSHIYA-MD/blob/main/Images/20251127_061223.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 OSHIYA-MD Is Alive Now owner+94756599952 🎭*",
+BOT_OWNER: '94756599952',  // Replace with the owner's phone number
 
 
 
